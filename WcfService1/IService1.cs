becee582c1123fd113b36b2a3885f16ba1f21140
Martin Kierkegaard <mark@easj.dk>
2018-04-04 19:39:50 +0200

    Fjernet alle default metoderne i SOAP servicen -Skrevet summary på InsertData metoden

--- WcfService1/IService1.cs
+++ WcfService1/IService1.cs
@@ -8,39 +8,11 @@ using System.Text;
 
 namespace WcfService1
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
         int InsertData(string request, double volume, double length, double width, double height);
-            
-
-
-
-
     }
 
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
--- WcfService1/Service1.svc.cs
+++ WcfService1/Service1.svc.cs
@@ -9,33 +9,22 @@ using System.Text;
 
 namespace WcfService1
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-
-
         //Husk at ændre i string så den peger på den rigtige database server, husk a indsætte user+password
         private const string ConnectionString =
           "Server=tcp:easj2016100.database.windows.net,1433;Initial Catalog=HotelDB;Persist Security Info=False;User ID='';Password='';MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+
+        /// <summary>
+        /// metode til at indsætte data i database på Azure 
+        /// </summary>
+        /// <param name="request">hvilken request er der tale om</param>
+        /// <param name="volume">den udregnede volumen</param>
+        /// <param name="length">længden </param>
+        /// <param name="width">bredden</param>
+        /// <param name="height">højden</param>
+        /// <returns>antal rækker der bliver berørt af sql'en (rowsaffected)</returns>
         public int InsertData(string request, double volume, double length, double width, double height)
         {
             const string sqlstring = "insert into volumen1 (request, volume, length, width, height) " +
